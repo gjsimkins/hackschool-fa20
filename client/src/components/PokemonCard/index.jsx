671a370f0c3f02da902c3c4f61324d0b423a11c2
gjsimkins <baseball3gary@gmail.com>
2020-11-19 17:43:12 -0800

    Deployment

--- client/src/components/PokemonCard/index.jsx
+++ client/src/components/PokemonCard/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import Move from '../Move';
+import CanvasDraw from 'react-canvas-draw';
 import './style.css';
 
-const PokemonCard = ({ name, description, img }) => {
+const PokemonCard = (props) => {
+    let pokemonMoves = props.moves.map((move, index) => {
+        return <Move key={index} name={move.name} type={move.type} power={move.power} />
+    });
+
     return (
         <div className="pokemon-card-container">
             <div className="pokemon-card-container-inner">
+                <CanvasDraw
+                    hideGrid={true}
+                    disabled={true}
+                    hideInterface={true}
+                    saveData={props.img}
+                />
                 <div className="pokemoncard-data-container">
-                    <p className="pokemoncard.name"> {name}</p>
-                    <img src={img} width="500"></img>
-                    <p className="pokemoncard.description">{ description }</p>
+                    <p className="pokemoncard.name"> {props.name}</p>
+                    <p className="pokemoncard.description">{props.description}</p>
+                    <p className="pokemoncard-bold">Moves :</p>
+                    <table className="moves-table">
+                        { pokemonMoves }
+                    </table>
                 </div>
             </div>
         </div>
